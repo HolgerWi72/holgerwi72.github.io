45b938dda6c9ca84dc0eff6e019bc2e58354ad73
HolgerWi72 <63157320+HolgerWi72@users.noreply.github.com>
2024-11-12 00:19:03 +0100

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,18 +1,15 @@
 
  //Custom Text Header //
- // ACSII slant font
 
 // Needs to be inside <section id="login" class="shadow">
-// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>
-  _            _       _          _           
- | |__   __ _ (_) __ _| |__   ___| |__   ___  
- | '_ \ / _` || |/ _` | '_ \ / _ \ '_ \ / _ \ 
- | |_) | (_| || | (_| | |_) |  __/ | | | (_) |
- |_.__/ \__,_|/ |\__,_|_.__/ \___|_| |_|\___/ 
-            |__/                              
+<div class="custom-text-header"> <pre>     
+  ________  ________   ____________________ __    __________  _________    __ _______
+ /_  __/ / / / ____/  / ____/ ____/ ____/ //_/   / ____/ __ \/ ____/   |  / //_/ ___/
+  / / / /_/ / __/    / / __/ __/ / __/ / ,<     / /_  / /_/ / __/ / /| | / ,<  \__ \ 
+ / / / __  / /___   / /_/ / /___/ /___/ /| |   / __/ / _, _/ /___/ ___ |/ /| |___/ / 
+/_/ /_/ /_/_____/   \____/_____/_____/_/ |_|  /_/   /_/ |_/_____/_/  |_/_/ |_/____/  
   </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
